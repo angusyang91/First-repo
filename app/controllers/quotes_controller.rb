7c66500e8e65b9df6d4f4618d0c1befccac3338a
Angus <angusyang91@gmail.comgit config --global user.name Angus>
2016-12-27 13:47:53 +0000

    Added validation rules for user submitted data

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,7 +8,10 @@ class QuotesController < ApplicationController
   end
 
   def create # All the code below this will execute when we click on Create
-    Quote.create(quote_params)
+    @quote = Quote.create(quote_params)
+    if @quote.invalid?
+      flash[:error] = '<strong> Could not save </strong> the data you entered is invalid.'
+    end
     redirect_to root_path
   end
 
